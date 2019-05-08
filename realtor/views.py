5353806a00ce0347d48806e5f0907b274f791716
Birta <birtab18@ru.is>
2019-05-08 12:48:15 +0000

    html filear i stað httpResponse

--- realtor/views.py
+++ realtor/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("<h2>Hello from index function within realtor!<h2>")
+    return render(request, 'realtor/index.html')
