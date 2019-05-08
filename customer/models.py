d64fba162ec07ef7419ca93792a7afa1c6f04c75
Björn Þorláksson <bjornt17@ru.is>
2019-05-08 17:05:42 +0000

    virkar að lesa ur data base i customer index

--- customer/models.py
+++ customer/models.py
@@ -8,6 +8,8 @@ class Customer(models.Model):
     phoneNR = models.IntegerField()
     address = models.CharField(max_length=255)
     email = models.CharField(max_length=255)
+    def __str__(self):
+        return self.name
 
 
 class CustomerImage(models.Model):
--- customer/views.py
+++ customer/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from customer.models import Customer
 
 # Create your views here.
 def index(request):
-    return render(request, 'customer/customer-index.html')
+    context = {'customers': Customer.objects.all().order_by('name')}
+    return render(request, 'customer/customer-index.html', context)
