62dfceda6411dc8830c79587d74bb6bf7704090f
Bergrós <bergros18@ru.is>
2019-05-09 12:13:06 +0000

    eyða gagnagrunn

--- customer/views.py
+++ customer/views.py
@@ -3,5 +3,5 @@ from customer.models import Customer
 
 # Create your views here.
 def index(request):
-    context = {'customers': Customer.objects.all().order_by('name')}
+    context = {'customers': 'hello'}
     return render(request, 'customer/customer-index.html', context)
