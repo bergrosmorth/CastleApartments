454bca6deb8aa397182bc6f9dead0caf8cd0e7ba
Bergrós <bergros18@ru.is>
2019-05-10 13:59:13 +0000

    apartment details

--- apartment/views.py
+++ apartment/views.py
@@ -1,7 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from apartment.models import Apartment
 
 
 def index(request):
     context = {'apartments': Apartment.objects.all().order_by('address')}
     return render(request, 'apartment/apartment-index.html', context)
+
+# /apartment/1
+def get_apartment_by_id(request, id):
+    return render(request, 'apartment/apartment_details.html', {
+        'apartment': get_object_or_404(Apartment, pk=id)
+    })
