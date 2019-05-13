28558a40905390ed061fb5355be3185e59d3b183
Bergrós <bergros18@ru.is>
2019-05-13 16:08:35 +0000

    user profile, avatar image

--- profile/models.py
+++ profile/models.py
@@ -1,15 +1,16 @@
 from django.db import models
 from django.contrib.auth.models import User
+from apartment.models import Apartment
+
 
 # Create your models here.
 
 
 class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     phone = models.IntegerField()
     address = models.CharField(max_length=255)
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    favorite_apartments = models.ForeignKey(Apartment, on_delete=models.CASCADE)
+    image = models.CharField( max_length=999 )
 
 
-class ProfileImage(models.Model):
-    image = models.CharField(max_length=999)
-    profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
--- profile/views.py
+++ profile/views.py
@@ -1,5 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from profile.models import Profile
+from profile.forms.profile_form import ProfileForm
 
 # Create your views here.
 def index(request):
     return render(request, 'profile/profile-index.html')
+
+
+def profile(request):
+        profile = Profile.objects.filter(user=request.user).first()
+        if request.method == 'POST':
+            form = ProfileForm(instance=profile, data=request.POST)
+            if form.is_valid():
+                profile = form.save(commit=False)
+                profile.user = request.user
+                profile.save()
+                return redirect('profile')
+        return render(request, 'profile/single_profile.html', {
+            'form': ProfileForm(instance=profile)
+        })
