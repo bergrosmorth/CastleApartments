19975ddc30778d1d1ffb947bec02dc7ed6a33abe
Bergrós <bergros18@ru.is>
2019-05-13 17:35:14 +0000

    update profile

--- profile/views.py
+++ profile/views.py
@@ -3,19 +3,17 @@ from profile.models import Profile
 from profile.forms.profile_form import ProfileForm
 
 # Create your views here.
-def index(request):
-    return render(request, 'profile/profile-index.html')
-
 
 def profile(request):
-        profile = Profile.objects.filter(user=request.user).first()
-        if request.method == 'POST':
-            form = ProfileForm(instance=profile, data=request.POST)
-            if form.is_valid():
-                profile = form.save(commit=False)
-                profile.user = request.user
-                profile.save()
-                return redirect('profile')
-        return render(request, 'profile/single_profile.html', {
-            'form': ProfileForm(instance=profile)
-        })
+    profile = Profile.objects.filter( user=request.user ).first()
+    if request.method == 'POST':
+        form = ProfileForm( instance=profile, data=request.POST )
+        if form.is_valid():
+            profile = form.save( commit=False )
+            profile.user = request.user
+            profile.save()
+            return redirect( 'profile' )
+    return render( request, 'profile/profile_index.html', {
+        'form': ProfileForm( instance=profile ),
+        'profile': profile
+    } )
