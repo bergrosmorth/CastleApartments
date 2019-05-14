701045c2456d673f847149a530f1b9fbbbcf6ae2
Birta <birtab18@ru.is>
2019-05-14 15:33:38 +0000

    reyna að setja fl en eina mynd i add apartment (virkar ekki)

--- apartment/forms/apartment_form.py
+++ apartment/forms/apartment_form.py
@@ -3,12 +3,11 @@ from django import forms
 from apartment.models import Apartment
 from profile.models import Profile
 
+
 class ApartmentAddForm(ModelForm):
-    #image = forms.CharField(required=True, widget=forms.TextInput(attrs={'class': 'form-control'}))
-    image = forms.FileField(widget=forms.ClearableFileInput(attrs={'multiple': True, 'class': 'form-control'})) #fá margar myndir virkar ekki
     class Meta:
         model = Apartment
-        exclude = [ 'id', 'realator']
+        exclude = ['id', 'realator']
         widgets = {
             'address': widgets.TextInput(attrs={'class': 'form-control'}),
             'zip': widgets.TextInput(attrs={'class': 'form-control'}),
@@ -37,18 +36,17 @@ class BuyProfile(ModelForm):
         model = Profile
         exclude = ['id']
 
-
 class ApartmentUpdateForm(ModelForm):
     class Meta:
         model = Apartment
         exclude = ['id', 'realator']
         widgets = {
-            'address': widgets.TextInput(attrs={'class': 'form-control'} ),
-            'zip': widgets.TextInput(attrs={'class': 'form-control'} ),
-            'city': widgets.TextInput(attrs={'class': 'form-control'} ),
-            'country': widgets.Select(attrs={'class': 'form-control'} ),
-            'size': widgets.NumberInput(attrs={'class': 'form-control'} ),
-            'price': widgets.NumberInput(attrs={'class': 'form-control'} ),
-            'rooms': widgets.NumberInput(attrs={'class': 'form-control'} ),
+            'address': widgets.TextInput(attrs={'class': 'form-control'}),
+            'zip': widgets.TextInput(attrs={'class': 'form-control'}),
+            'city': widgets.TextInput(attrs={'class': 'form-control'}),
+            'country': widgets.Select(attrs={'class': 'form-control'}),
+            'size': widgets.NumberInput(attrs={'class': 'form-control'}),
+            'price': widgets.NumberInput(attrs={'class': 'form-control'}),
+            'rooms': widgets.NumberInput(attrs={'class': 'form-control'}),
             'description': widgets.TextInput(attrs={'class': 'form-control'}),
         }
