111f96ff7c56b93a67ec4300e063596d9bc53cc8
Dagur <dagurh16@ru.is>
2019-05-16 00:18:40 +0000

    rangt lykilorð villumeðhöndlun

--- login/urls.py
+++ login/urls.py
@@ -1,11 +1,22 @@
 from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.forms import AuthenticationForm
 from django.urls import path
 from . import views
 
 from apartment.models import Apartment
 
 
+class AuthFormWithNewMessage(AuthenticationForm):
+    error_messages = {
+        'invalid_login':
+            "HAHAHAHAAH! Please enter a correct %(username)s and password. Note that both fields may be case-sensitive.",
+        'inactive': "This account is inactive."
+    }
+
+
 class LoginViewWithApartments(LoginView):
+    form_class = AuthFormWithNewMessage
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
@@ -13,6 +24,8 @@ class LoginViewWithApartments(LoginView):
         new_context = super(LoginView, self).get_context_data(**kwargs)
         new_context['apartments'] = Apartment.objects.all()
         return new_context
+
+
 
 
 urlpatterns = [
